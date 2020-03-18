7c67fe63ed3e1291afd1317cd3ea9b81ff2d16f3
Hanmin Kim <hanminkim123@gmail.com>
2020-03-18 15:46:17 -0700

    no merge conflicts \o/ (#1)
    * randomness works i guess
    
    * removed do cause redundant
    
    * randomness fixed and sksk
    
    * theres ur whitespaces u sksk
    
    * white spaces
    
    * sksk
    
    * added local variables, return at bottom
    
    * did ur changes

--- random.js
+++ random.js
@@ -1,9 +1,19 @@
+var playedSongs = [];
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    const lmin = Math.ceil(min);
+    const lmax = Math.floor(max);
+    var i = Math.floor(Math.random() * (lmax - lmin + 1)) + lmin;
+
+    while (playedSongs.length > lmax / 2) {
+        playedSongs.shift()
+    }
+    while (playedSongs.includes(i)) {
+        i = Math.floor(Math.random() * (lmax - lmin + 1)) + lmin;
+    }
+    playedSongs.push();
+    return i
 }
 
 module.exports = {
-  getRandomIntInclusive,
+    getRandomIntInclusive,
 }
