3a8a379087a40f2f6d8b8812ac2199ab1f79b1b9
Hanmin Kim <hanminkim123@gmail.com>
2020-03-18 15:15:45 -0700

    randomness works i guess

--- random.js
+++ random.js
@@ -1,7 +1,40 @@
+var playedSongs = []
+
+
+
+
+
+
+
+
+
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    i = null
+
+    while (playedSongs.length > max/2){
+      playedSongs.shift()
+    }
+
+
+
+
+
+
+
+    do{
+      i = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    while (!playedSongs.includes(i)){
+      i = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    return i
+
+
+
+
+    playedSongs.push();
 }
 
 module.exports = {
