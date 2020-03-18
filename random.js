9e26240170c9be125a824c368498bc0b714d7785
Hanmin Kim <hanminkim123@gmail.com>
2020-03-18 15:30:09 -0700

    sksk

--- random.js
+++ random.js
@@ -2,12 +2,12 @@ var playedSongs = [];
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    i = null
+    i = 0
 
     while (playedSongs.length > max / 2) {
         playedSongs.shift()
     }
-    while (playedSongs.includes(i)) {
+    while (playedSongs.includes(i) | i == 0) {
         i = Math.floor(Math.random() * (max - min + 1)) + min;
     }
     return i
