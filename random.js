53ef56a616a76b2c5704619b0f1d766185408b19
shizubro <michelleliu.ca@gmail.com>
2020-03-18 17:19:49 -0700

    fix random

--- random.js
+++ random.js
@@ -1,16 +1,15 @@
 var playedSongs = [];
+
 function getRandomIntInclusive(min, max) {
-    const lmin = Math.ceil(min);
-    const lmax = Math.floor(max);
-    var i = Math.floor(Math.random() * (lmax - lmin + 1)) + lmin;
+    var i = Math.floor(Math.random() * (max - min + 1)) + min;
 
     while (playedSongs.length > lmax / 2) {
         playedSongs.shift();
     }
     while (playedSongs.includes(i)) {
-        i = Math.floor(Math.random() * (lmax - lmin + 1)) + lmin;
+        i = Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    playedSongs.push();
+    playedSongs.push(i);
     return i
 }
 
