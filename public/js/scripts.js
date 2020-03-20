708edb70536a1cfcadcc9fdbd310a14754f253d5
shizubro <michelleliu.ca@gmail.com>
2020-03-19 22:07:03 -0700

    change

---
+++ public/js/scripts.js
@@ -0,0 +1,86 @@
+var src = "";
+var name = "";
+var artist = "";
+var album = "";
+var cover = "";
+var next = "";
+var time = 0;
+var volume = 1;
+var sound = null;
+var nextSound = null;
+mute = false;
+start = false;
+
+function backendMute(muteState){
+    mute = muteState;
+}
+function backstarted(){
+    start = true;
+}
+
+function getSong() {
+    $.ajax({
+        url: "/metadata",
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result){
+            src = result['url'];
+            name = result['name'];
+            artist = result['artist'];
+            album = result['album'];
+            cover = result['cover'];
+            next = result['next'];
+
+            if (!sound) {
+                sound = new Howl({
+                    src: [src],
+                    volume: volume
+                })
+            }
+            nextSound = new Howl({
+                src: [next],
+                volume: volume
+            })
+
+            sound.on("load", function () {
+                document.getElementById("loading").style.display = "none";
+            });
+
+            playSong();
+        }
+    });
+}
+
+function playSong() {
+    $.ajax({
+        url: "/time",
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result){
+            time = result['time'];
+
+            document.getElementById("song-title").innerHTML = name;
+            document.getElementById("song-artist").innerHTML = artist;
+            document.getElementById("song-album").innerHTML = album;
+            document.getElementById("album-cover").src = cover;
+            
+            if(start){
+                // console.log(sound);
+                // console.log(nextSound);
+                sound.seek(time);
+                sound.play();
+                sound.on("end", nextSong);
+                if(mute){
+                    sound.volume(0);
+                }
+            }
+        }
+    });
+}
+
+function nextSong() {
+    sound.unload();
+    nextSound.volume = sound.volume;
+    sound = nextSound;
+    getSong();
+}
--- server.js
+++ server.js
@@ -7,10 +7,15 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(express.static(__dirname + '/public'));
 
 app.get("/metadata", db.getMetadata);
 
 app.get("/time", db.getTime);
+
+app.get("/", function(req, res) {
+    res.sendFile( path.join(__dirname, 'public', 'index.html'));
+})
 
 app.listen(3000, () => {
     console.log("Server running on port");
